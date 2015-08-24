a26729a22b58715c2f1c6f88965326135427d345
shimon <shimon.magal@gmail.com>
2015-08-24 22:12:40 +0300

    initial commit

---
+++ src/RTC2GIT.java
@@ -0,0 +1,11 @@
+/**
+ * Created by shimon on 24/08/15.
+ *
+ * RTC2GIT - Helps you convert an RTC sandbox to a git local repository,
+ * by finding all *.jazzignore files and converting it into one *.gitginore file.
+ *
+ * Notice: It doesn't actually convert a
+ */
+public class RTC2GIT {
+
+}
